e60b4d856d8f3e6f0453eba71678eb2e886e848c
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2020-12-20 22:36:10 +1000

    cleaned up a bit

--- run.bat
+++ run.bat
@@ -2,13 +2,21 @@
 rem Config stuff:
 set VER= 1.2
 set DCOL= 07
-set MDCNT=W.I.P
+set MDCNT=0
 set COLA= 09
 set COLB= 0A
 set COLC= 07
 title Tehbb's Modpack Installer [Ver %VER%]
-rem Program start:
 set q=^"
+rem Program start:
+rem Count bundled mods: seems broken currently
+rem cd %cd%\data\mods
+rem for /f %%A in ('dir ^| find "File(s)"') do set MDCNT=%%A
+rem for /f %%A in ('dir "%appdata%\.minecraft\mods" ^| find "File(s)"') do set IMDNT=%%A
+rem cd %~dp0
+rem echo MDCNT %MDCNT%
+rem echo IMDCNT %IMDNT%
+:pstart
 rem call :c 0a "[Debug]"&call :c %DECOL% "b"&call :c %DCOL% "c" /n&call :cb
 call :c %COLC% "=================================================================================" /n
 call :c %COLA% "  ______     __    __    __   _      "&call :c 0A "    __  ___          __                 __  " /n
@@ -38,9 +46,6 @@ echo Have you installed the correct version of forge yet? [Type "y" or "n"]
 set /P c=
 if /I "%c%" EQU "y" goto :sw
 if /I "%c%" EQU "n" goto :swa
-if /I "%c%" EQU "pog" goto :pog
-if /I "%c%" EQU "skp" goto :cpm
-if /I "%c%" EQU "cool berd" goto :pic
 if /I "%c%" EQU "console" goto :nconsole
 
 if defined dbg (
@@ -81,8 +86,7 @@ call :c 02 "   _/  .!q!    "&call :c 05 "{  -'.~('-._,.'"&call :c 02 "\_,/" /n
 call :c 02 "  /  .!q!    _/'"&call :c 05 "`--; ;  `.  ;" /n
 call :c 02 "   .=!q!  _/'      "&call :c 05 "`-..__,-'" /n
 call :c 02 "    __/'" /n&call :cb
-pause
-@ goto :choice
+@ goto :console
 :pog
 cls
 title POGGERS POG POGERATOR!!!!![Ver %VER%]
@@ -114,7 +118,6 @@ IF EXIST "%appdata%\.minecraft\mods" (
   set /P c=Do you want to clear your mods directory before installing the new mods? [Type "y" or "n"]
   if /I "%c%" EQU "y" goto :delm
   if /I "%c%" EQU "n" goto :cpm
-  if /I "%c%" EQU "pog" goto :pog
 ) ELSE (
   @ goto :cpm
 )
@@ -134,12 +137,15 @@ IF EXIST "%appdata%\.minecraft\mods" (
 :cpm
 echo Press any key to copy the modpack into your game directory
 pause >nul
-cls
 if defined dbg (
+cls
 call :c 0a "[Debug]"&call :c %DCOL% "Current error code [%ERRORLEVEL%]" /n &call :cb
 robocopy "%cd%\data\mods" "%appdata%\.minecraft\mods" /e
 call :c 0a "[Debug]"&call :c %DCOL% "Current error code [%ERRORLEVEL%]" /n &call :cb
 ) else (
+echo Copying...
+ROBOCOPY "%cd%\data\mods" "%appdata%\.minecraft\mods"  /NDL /NJH /NJS /np /NFL
+cls
 call :c %COLC% "=================================================================================" /n
 call :c %COLA% "  ______     __    __    __   _      "&call :c 0A "    __  ___          __                 __  " /n
 call :c %COLA% " /_  __/__  / /_  / /_  / /_ ( )_____"&call :c 0A "   /  |/  /___  ____/ /___  ____ ______/ /__" /n
@@ -152,7 +158,6 @@ call :c %COLC% " _/ // / / (__  ) /_/ /_/ / / /  __/ /                          
 call :c %COLC% "/___/_/ /_/____/\__/\__,_/_/_/\___/_/                             "&call :c %DCOL% " Version%VER%  " /n
 call :c %COLC% "=================================================================================" /n&call :cb
 for /l %%i in (1,1,11) do echo:
-ROBOCOPY "%cd%\data\mods" "%appdata%\.minecraft\mods"  /NDL /NJH /NJS /np /NFL
 )
 echo Now open the minecraft launcher select the forge 1.12.2 profile than run the game
 echo When you are in the main menu of the game it should say [%MDCNT%] mods active in the bottom left of the screen
@@ -162,7 +167,6 @@ echo Cheers!
 echo Press any key to exit
 pause >nul
 exit
-
 rem setup new console session:
 :nconsole
 cls
@@ -178,93 +182,95 @@ call :c %COLC% " _/ // / / (__  ) /_/ /_/ / / /  __/ /                          
 call :c %COLC% "/___/_/ /_/____/\__/\__,_/_/_/\___/_/                             "&call :c %DCOL% " Version%VER%  " /n
 call :c %COLC% "=================================================================================" /n&call :cb
 @ goto :console
-
 rem run console itself:
 :console 
 set "c="
 call :c %DCOL% ">"&call :cb
 set /P c=
-
 rem COMMANDS:
-
-
-
 if /I "%c%" EQU "debug" (
-  if defined dbg (
-    set "dbg="
-    call :c 0a "[Debug]"&call :c %DCOL% "Debug disabled" /n &call :cb
-  ) else (
-    set "dbg=y"
-    call :c 0a "[Debug]"&call :c %DCOL% "Debug enabled" /n &call :cb
-  )
+	if defined dbg (
+		set "dbg="
+		call :c 0a "[Debug]"&call :c %DCOL% "Debug disabled" /n &call :cb
+	) else (
+		set "dbg=y"
+		call :c 0a "[Debug]"&call :c %DCOL% "Debug enabled" /n &call :cb
+	)
 ) else (
-  if /I "%c%" EQU "help" (
-    goto :h
-  ) else (
-    if /i "%c:~0,2%"=="ls" (
-      call :ls %c%
-    ) else (
-      if /I "%c%" EQU "cls" (
-        cls
-      ) else (
-        if /I "%c%" EQU "fresh" (
-          @ goto :nconsole
-        ) else (
-          if /I "%c%" EQU "compare" (
-            @ goto :compare
-          ) else (
-            call :c 0A "[TMI]"&call :c %DCOL% "Unknown or invalid command" /n
-          )
-        )
-      )
-    )
-  )
+	if /I "%c%" EQU "help" (
+		goto :h
+	) else (
+		if /i "%c:~0,2%"=="ls" (
+			call :ls %c%
+		) else (
+			if /I "%c%" EQU "cls" (
+				cls
+			) else (
+				if /I "%c%" EQU "fresh" (
+					goto :nconsole
+				) else (
+					if /I "%c%" EQU "compare" (
+						goto :compare
+					) else (
+						if /I "%c%" EQU "el" (
+							call :c 0a "[Debug]"&call :c %DCOL% "Current error code [%ERRORLEVEL%]" /n &call :cb
+						) else (
+							if /I "%c%" EQU "exit" (
+								cls
+								goto :pstart
+							) else (
+								if /I "%c%" EQU "pog" (
+									goto :pog
+								) else (
+									if /I "%c%" EQU "cool berd" (
+										goto :pic
+									) else (
+										call :c 0A "[TMI]"&call :c %DCOL% "Unknown or invalid command" /n       
+									)
+								)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
 )
-
-
-
-
-
+if /I "%c%" EQU "mcnt" (
+	call :c 0a "[TMI]"&call :c %DCOL% "Bundled mods   [%MDCNT%]" /n
+	call :c 0a "[TMI]"&call :c %DCOL% "Installed mods [%IMDNT%]" /n &call :cb
+) else (
 rem call :c 0A "[TMI]"&call :c %DCOL% "Unknown or invalid command" /n
-
-
 @ goto :console 
-
-
-
 :compare
 SetLocal EnableDelayedExpansion
 set "folder1=%cd%\data\mods"
 set "folder2=%appdata%\.minecraft\mods"
 For /R "%Folder1%" %%x In (*.*) Do (
-    Set "FullPath=%%x"
-    Set "RelPath=!FullPath:%Folder1%=!"
-    If Exist "%Folder2%!RelPath!" (
-      >Nul 2>&1 FC /b "%Folder1%!RelPath!" "%Folder2%!RelPath!" && (
-       rem Echo Dup - %%x
-       call :c 20 " DUP "&call :c %DCOL% " - %%x" /n&call :cb
-      )||(
-       rem Echo Dif - %%x
-       call :c 60 " DIF "&call :c %DCOL% " - %%x" /n&call :cb
-      )
-    ) Else (
-      rem Echo New - %%x
-      call :c 60 " NEW "&call :c %DCOL% " - %%x" /n&call :cb
-    )
+	Set "FullPath=%%x"
+	Set "RelPath=!FullPath:%Folder1%=!"
+	If Exist "%Folder2%!RelPath!" (
+	  >Nul 2>&1 FC /b "%Folder1%!RelPath!" "%Folder2%!RelPath!" && (
+	   rem Echo Dup - %%x
+	   call :c 20 " DUP "&call :c %DCOL% " - %%x" /n&call :cb
+	  )||(
+	   rem Echo Dif - %%x
+	   call :c 60 " DIF "&call :c %DCOL% " - %%x" /n&call :cb
+	  )
+	) Else (
+	  rem Echo New - %%x
+	  call :c 60 " NEW "&call :c %DCOL% " - %%x" /n&call :cb
+	)
 )
 For /R "%Folder2%" %%x In (*.*) Do (
-    Set "FullPath=%%x"
-    Set "RelPath=!FullPath:%Folder2%=!"
-    If Not Exist "%Folder1%!RelPath!" (
-      rem Echo New - %%x
-      call :c 60 " NEW "&call :c %DCOL% " - %%x" /n&call :cb
-    )
+	Set "FullPath=%%x"
+	Set "RelPath=!FullPath:%Folder2%=!"
+	If Not Exist "%Folder1%!RelPath!" (
+	  rem Echo New - %%x
+	  call :c 60 " NEW "&call :c %DCOL% " - %%x" /n&call :cb
+	)
 )
 @ goto :console 
-
-
-
-
 :ls
 if defined dbg (
   call :c 0a "[Debug]"&call :c %DCOL% "Command perameters: %~1 %~2" /n &call :cb
@@ -278,68 +284,48 @@ if /I "%~2" EQU "b" (
   dir "%cd%\data\mods" /W /B 
 ) else (
   if /I "%~2" EQU "i" (
-    echo:
-    echo Currently installed mods:
-    echo =================================================================================
-    echo:
-    dir "%appdata%\.minecraft\mods" /W /B
+	echo:
+	echo Currently installed mods:
+	echo =================================================================================
+	echo:
+	dir "%appdata%\.minecraft\mods" /W /B
   ) else (
-    if /I "%~2" EQU "" (
-      call :c 0A "[TMI]"&call :c %DCOL% "Command requires a perameter" /n
-      call :c 0A "     "&call :c %DCOL% "Available Perameters: bundled [b] or installed [i]" /n
-      call :c 0A "     "&call :c %DCOL% "E.g:[ ls i ] will list installed mods" /n
-    ) else (
-      call :c 0A "[TMI]"&call :c %DCOL% "Invalid perameter:%~2" /n
-      call :c 0A "     "&call :c %DCOL% "Available Perameters: bundled [b] or installed [i]" /n
-      call :c 0A "     "&call :c %DCOL% "E.g:[ ls i ]" /n
-    )
+	if /I "%~2" EQU "" (
+	  call :c 0A "[TMI]"&call :c %DCOL% "Command requires a perameter" /n
+	  call :c 0A "     "&call :c %DCOL% "Available Perameters: bundled [b] or installed [i]" /n
+	  call :c 0A "     "&call :c %DCOL% "E.g:[ ls i ] will list installed mods" /n
+	) else (
+	  call :c 0A "[TMI]"&call :c %DCOL% "Invalid perameter:%~2" /n
+	  call :c 0A "     "&call :c %DCOL% "Available Perameters: bundled [b] or installed [i]" /n
+	  call :c 0A "     "&call :c %DCOL% "E.g:[ ls i ]" /n
+	)
   )
 )
-
-    
-
-
-
-
-
-
 @ goto :console
-
-
-
-
-
-
-
-
-
-
 :h
 call :c %DCOL% " =============================================================================== " /n
 call :c %DCOL% " |                                  Help                                       | " /n
 call :c %DCOL% " =============================================================================== " /n
 call :c %DCOL% "Commands:                                                                        " /n
 call :c %DCOL% "    help      - Lists commands and functions                                     " /n
-call :c %DCOL% "    ls        - " /n
-call :c %DCOL% "    el        - Outputs current error level" /n
+call :c %DCOL% "    exit      - Exits the console                                                " /n
+call :c %DCOL% "    ls        - Lists a mod directory use i or b parameters                      " /n
+call :c %DCOL% "    el        - Outputs current error level                                      " /n
+call :c %DCOL% "    compare   - Compares the mods bundled and the mods installed                 " /n
+call :c %DCOL% "    debug     - Toggles debug mode                                               " /n
+call :c %DCOL% "    fresh     - Resets the console                                               " /n
+call :c %DCOL% "    cls       - Clears the console                                               " /n
+rem call :c %DCOL% "    mcnt      - Displays the number of installed and bundled mods" /n
 call :c %DCOL% "    " /n
 call :c %DCOL% "    " /n
-call :c %DCOL% "    " /n
-call :c %DCOL% "    " /n
-call :c %DCOL% "    " /n
-call :c %DCOL% "    " /n
-call :c %DCOL% "    " /n
-call :c %DCOL% "    Note: This is unfinished" /n
+call :c %DCOL% "    Note: More features are still to be added" /n
 call :cb
 @ goto :console
-
-
-
 rem colored text stuff:
 :cb
 if exist "%temp%\color.psm1" (
-    powershell -command "&{Import-Module '%temp%\color.psm1'}"
-    del "%temp%\color.psm1"
+	powershell -command "&{Import-Module '%temp%\color.psm1'}"
+	del "%temp%\color.psm1"
 )
 exit /b
 :c <color pair> <string> </n>
